aaff408b50648ff10aa1504a8b34311531a0547b
unknown <admin@HP-1GTJUIM.uhwi.org.jm>
2021-02-09 02:51:15 -0500

    certs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const http = require('http');
 var express = require('express');
 var compression = require('compression');
 var path = require("path");
 var app = express();
+var fs = require('fs');
 app.use(express.static('public'));
 app.use(express.static('files'));
 app.use(compression());
 
-app.listen(process.env.PORT || 80, function(){
+var privateKey = fs.readFileSync( 'key.key' );
+var certificate = fs.readFileSync( 'certificate.pem' );
+
+http.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(process.env.PORT || 80,function(){
     console.log("webserver listening!");
 });
